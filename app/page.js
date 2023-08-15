7ce2f4726c3caed4ded8a970bd1c005c98bdd6d9
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-15 14:29:03 +0600

    done

--- app/page.js
+++ app/page.js
@@ -1,10 +1,29 @@
 import React from "react";
 import styles from "./home.module.css";
+import postData from "../app/api/getPosts.js";
+import Button from "./components/Button";
 
-const page = () => {
+const page = async () => {
+  const allPosts = await postData();
+  console.log(allPosts);
   return (
     <main>
-      <h1>Home Page</h1>
+      <div className={styles.sectionHeader}>
+        <h1 className={styles.header}>Recent Post</h1>
+      </div>
+      <div>
+        {allPosts.map((post) => {
+          return (
+            <div key={post.id} className={styles.card}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+              <div>
+                <Button>Read More</Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 };
