7b40f1cd5c5655fb17553b9f09bab298666f9fb4
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-17 11:03:17 +0600

    active color set done

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,9 +1,12 @@
+"use client";
 import React from "react";
 import navData from "../utils/navData";
 import Link from "next/link";
 import styles from "./componets.module.css";
+import { useParams, usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
@@ -12,8 +15,17 @@ const Header = () => {
         </div>
         <div className={styles.links}>
           {navData.map((link) => {
+            const isActive = pathname === link.href;
             return (
-              <Link href={link.href} key={link.id}>
+              <Link
+                className={
+                  isActive
+                    ? " text-orange-600 underline underline-offset-2"
+                    : " text-green-600"
+                }
+                href={link.href}
+                key={link.id}
+              >
                 {link.name}
               </Link>
             );
